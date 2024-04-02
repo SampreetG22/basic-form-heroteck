8b121d219267417f671b9867cdcd52c99bba1777
SampreetG <sampreet.gumparla@nextinvo.com>
2024-04-02 23:15:34 +0530

    Updated readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# basic-form-heroteck
+# Heroteck-basic-form-submission-using-React and Node.js
+
+# Run and setup instructions
+
+1. After cloning the repository, enter **cd client** and once you are in client directory, run **npm start** to start local **frontend server**.
+2. Repeat the same for **server** folder to start **backend server**.
--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const PORT = 5000;
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 
-app.post('/submit-form', (req, res) => {
-  const { name, email, password, phoneNumber } = req.body;
-  console.log(name, email, password, phoneNumber);
-  res.status(200).send({ message: 'Form submitted successfully!' });
+app.post("/submit-form", (req, res) => {
+  res
+    .status(200)
+    .send({ user_data: req.body, message: "Form submitted successfully!" });
 });
 
 app.listen(PORT, () => {
